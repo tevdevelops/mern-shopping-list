c4c9922406e1d861f0a60f1a248e8ece411b119d
tevdevelops <tevinrivera94@gmail.com>
2019-03-15 23:02:45 -0400

    Fixed heroku server bug in server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === "production") {
   // Set Static Folder
   app.use(express.static("client/build"));
 
-  app.length("*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
